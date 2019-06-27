13caf700253732a759c905ce66bf2d5d07635442
Muhammad Risky Makira <kirahaxor20@gmail.com>
2019-06-28 02:11:17 +0700

    Initial Commit

---
+++ SGBTeam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SGBTeam
+//
+//  Created by Payment Development on 28/06/19.
+//  Copyright © 2019 SGB Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
