5f895c0ed255745fada45aa43385e0174eae8520
Muhammad Risky Makira <kirahaxor20@gmail.com>
2019-06-28 02:42:47 +0700

    first commit

--- SGBTeam/ViewController.swift
+++ SGBTeam/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    let webView: WKWebView = {
+        let view = WKWebView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.sgb.or.id/appversion/")
+        let myWebsite = URLRequest(url: url!)
+        
+        webView.load(myWebsite)
+        
+        view.addSubview(webView)
+        setupWebViewScreen()
     }
-
-
+    func setupWebViewScreen(){
+        webView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        webView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        webView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        webView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        
+    }
+    
+    
 }
-
